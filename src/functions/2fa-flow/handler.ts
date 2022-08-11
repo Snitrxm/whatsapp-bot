15ec2007cf0ba3d9cc8b4d65a91450e9ba488caf
Snitrxm <andre.oliveira.roch@hotmail.com>
2022-08-11 18:26:23 -0300

    add query string parameters

--- src/functions/2fa-flow/handler.ts
+++ src/functions/2fa-flow/handler.ts
@@ -6,15 +6,26 @@ import stringToTitle from "../../libs/utils/stringToTitle";
 export const start_2fa_flow = async (event: APIGatewayEvent) => {
   const httpRequestAdapter = new HttpRequestAdapter();
 
-  const { phone }: any = event.body;
-  try {
-    await axios.post(`https://api.z-api.io/instances/3A9CB9A99735802F7A4262D448F3906A/token/E30A54EB98C1D37EA9648FE7/send-text`, {
-      phone,
-      message: "Test bot",
-    })
-  } catch (error) {
-    console.log(error);
-  }
+  const { phone, name }: any = event.queryStringParameters;
+  await httpRequestAdapter.post(`${process.env.ZAPI_BASE_URL}/send-button-list`, undefined, {
+    phone,
+    message: `Opa, tudo bom? Aqui é o *Bruno*, da plataforma MundoInvest e estou aqui para te ajudar!  Só para confirmar, você é o ${stringToTitle(
+      String(name).split(" ")[0]
+    )}?`,
+    buttonList: {
+      buttons: [
+        {
+          id: "cadastroSIM",
+          label: "Sim, sou eu!",
+
+        },
+        {
+          id: "cadastroNAO",
+          label: "Não, não sou!",
+        },
+      ],
+    },
+  });
   
   return formatJSONResponse({
     statusCode: 200,
